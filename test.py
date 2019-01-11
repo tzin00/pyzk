1747b3a6cc4806c77cda8cfe5c0c182688e374e3
Thant Zin Oo <annaskhan00@gmail.com>
2019-01-11 16:55:23 +0630

    nn

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ from zk import ZK, const
 sys.path.append("zk")
 
 conn = None
-zk = ZK('192.168.1.10', port=4370, timeout=5)
+zk = ZK('172.16.0.4', port=4370, timeout=5)
 try:
-    print 'Connecting to device ...'
+    print ('Connecting to device ...')
     conn = zk.connect()
-    print 'Disabling device ...'
+    print ('Disabling device ...')
     conn.disable_device()
-    print 'Firmware Version: : {}'.format(conn.get_firmware_version())
+    print ('Firmware Version: : {}'.format(conn.get_firmware_version()))
     # print '--- Get User ---'
     users = conn.get_users()
     for user in users:
@@ -20,19 +20,19 @@ try:
         if user.privilege == const.USER_ADMIN:
             privilege = 'Admin'
 
-        print '- UID #{}'.format(user.uid)
-        print '  Name       : {}'.format(user.name)
-        print '  Privilege  : {}'.format(privilege)
-        print '  Password   : {}'.format(user.password)
-        print '  Group ID   : {}'.format(user.group_id)
-        print '  User  ID   : {}'.format(user.user_id)
+        print ('- UID #{}'.format(user.uid))
+        print ('  Name       : {}'.format(user.name))
+        print ('  Privilege  : {}'.format(privilege))
+        print ('  Password   : {}'.format(user.password))
+        print ('  Group ID   : {}'.format(user.group_id))
+        print ('  User  ID   : {}'.format(user.user_id))
 
-    print "Voice Test ..."
-    conn.test_voice()
-    print 'Enabling device ...'
+    # print "Voice Test ..."
+    # conn.test_voice()
+    # print 'Enabling device ...'
     conn.enable_device()
-except Exception, e:
-    print "Process terminate : {}".format(e)
+except Exception as e:
+    print ("Process terminate : {}".format(e))
 finally:
     if conn:
         conn.disconnect()
--- test_user_enrollment.py
+++ test_user_enrollment.py
@@ -11,8 +11,8 @@ zk = ZK('192.168.1.201', port=4370, timeout=5)
 try:
   conn = zk.connect()
   conn.disable_device()
-  zk.set_user(26, 'Shubhamoy Chakrabarty', 0, '', '1', '26')
-  zk.enroll_user('26')
+  # zk.set_user(26, 'Shubhamoy Chakrabarty', 0, '', '1', '26')
+  # zk.enroll_user('26')
   conn.enable_device()
 except Exception, e:
     print "Process terminate : {}".format(e)
